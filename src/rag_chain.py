0cf9ee7ea683b5244270c3665a5fbe40f32874f0
vipthunder <166924573+vipthunder@users.noreply.github.com>
2026-05-30 23:40:20 +0530

    Update rag_chain.py
    Refactored RAG chain implementation to use langchain chain format.

--- src/rag_chain.py
+++ src/rag_chain.py
@@ -1,41 +1,3 @@
-# from langchain.chains import create_retrieval_chain
-# from langchain.chains.combine_documents import create_stuff_documents_chain
-
-# from langchain_core.prompts import ChatPromptTemplate
-
-# from src.llm import llm
-
-
-# def create_rag_chain(retriever):
-
-#     prompt = ChatPromptTemplate.from_template(
-#         """
-#         You are an AI assistant.
-
-#         Answer the question using only the provided context.
-
-#         Context:
-#         {context}
-
-#         Question:
-#         {input}
-#         """
-#     )
-
-#     document_chain = create_stuff_documents_chain(
-#         llm,
-#         prompt
-#     )
-
-#     retrieval_chain = create_retrieval_chain(
-#         retriever,
-#         document_chain
-#     )
-
-#     return retrieval_chain
-
-# instead of this o am using langchain chain format like
-
 from operator import itemgetter
 
 from langchain_core.prompts import ChatPromptTemplate
@@ -70,9 +32,7 @@ def create_rag_chain(retriever):
     rag_chain = (
         {
             "context":
-                itemgetter("question")
-                | retriever
-                | format_docs,
+                itemgetter("question")| retriever| format_docs,   # chain
 
             "question":
                 itemgetter("question")
