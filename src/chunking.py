62146dd15388a37d881bd47f701b77ae4b1256eb
Vipul Singh <166924573+vipthunder@users.noreply.github.com>
2026-05-31 13:21:48 +0530

    Refactor chunk_text to handle multiple segments

--- src/chunking.py
+++ src/chunking.py
@@ -1,12 +1,26 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+
 def chunk_text(segments):
-    
+
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200
     )
-    
-    chunks = splitter.split_text(text)
+
+    chunks = []
+
+    for segment in segments:
+
+        sub_chunks = splitter.split_text(segment["text"])
+
+        for chunk in sub_chunks:
+            chunks.append(
+                {
+                    "text": chunk,
+                    "start": segment["start"],
+                    "end": segment["end"]
+                }
+            )
+
     return chunks
-    
